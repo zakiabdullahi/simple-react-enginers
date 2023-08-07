f4e25f604b5465a26ab9052897c890ab5863ba31
Zaki abdullahi <qable2121@gmail.com>
2023-08-07 23:59:00 +0300

    fixed

--- src/components/Engineer.jsx
+++ src/components/Engineer.jsx
@@ -1,12 +1,8 @@
 /* eslint-disable react/prop-types */
 const Engineer = ({ engineer }) => {
   return (
-    <div className="w-full flex flex-col justify-center items-center border border-gray-400 w-72 rounded rounded-sm">
-      <img
-        style={{ width: "200px", height: "200px", borderRadius: "50%" }}
-        src={engineer.imageUrl}
-        alt=""
-      />
+    <div className=" flex flex-col justify-center items-center border border-gray-400 w-72 rounded rounded-sm">
+      <img className="w-40 h-36 rounded-full" src={engineer.imageUrl} alt="" />
       <h3>{engineer.name}</h3>
       <p>{engineer.role}</p>
     </div>
